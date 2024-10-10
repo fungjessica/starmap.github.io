a3c3e7dac12501e794a6e59da03fe64ecba8a87a
Lee Rogers <lrogers0941@gmail.com>
2024-10-10 01:25:52 -0700

    Added basic floating hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
-import './App.css';
+import HamburgerMenu from "./components/HamburgerMenu/HamburgerMenu";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>hi</p>
-        
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <HamburgerMenu></HamburgerMenu>
+         <p>hi</p>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Twirl } from "hamburger-react";
+import "./HamburgerMenu.css";
+
+const HamburgerMenu = () => {
+   const [isOpen, setOpen] = useState(false);
+
+   return (
+      <>
+         <div className="hamburgermenu-container">
+            <div className={isOpen ? "menu-active" : "menu"}>
+               {/*REPLACE -- Add function capabilities here once available*/}
+               <p className="item">Awesome option #1</p>
+               <p className="item">Awesome option #2</p>
+               <p className="item">Awesome option #3</p>
+               <p className="item">Awesome option #4</p>
+            </div>
+            <div className="icon-container">
+               <Twirl
+                  rounded
+                  color="#ffffff"
+                  toggled={isOpen}
+                  toggle={setOpen}
+               ></Twirl>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default HamburgerMenu;
